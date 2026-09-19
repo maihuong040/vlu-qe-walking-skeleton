04c656c2baa82257023fdb90ccad316e3ef00586
maihuong040 <daomaihuong040@gmail.com>
2026-09-19 10:22:11 +0700

    Create login.js

---
+++ login.js
@@ -0,0 +1,19 @@
+const { login } = require("./login");
+
+describe("Login function", () => {
+  test("Trả về true khi username và password đúng", () => {
+    expect(login("admin", "123")).toBe(true);
+  });
+
+  test("Trả về false khi username sai", () => {
+    expect(login("user", "123")).toBe(false);
+  });
+
+  test("Trả về false khi password sai", () => {
+    expect(login("admin", "456")).toBe(false);
+  });
+
+  test("Trả về false khi cả username và password sai", () => {
+    expect(login("user", "456")).toBe(false);
+  });
+});
